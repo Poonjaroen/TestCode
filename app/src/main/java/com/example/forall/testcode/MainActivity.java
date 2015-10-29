263e78a93cf160a432ba0bfca494924564d77b18
Wasit Poonjaroen <wasitfang@gmail.com>
2015-10-29 21:37:05 +0700

    test commit with source tree

--- app/src/main/java/com/example/forall/testcode/MainActivity.java
+++ app/src/main/java/com/example/forall/testcode/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity implements OneFragment.OnFra
 
     int i = 0;
 
+    String name = "TEST";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
